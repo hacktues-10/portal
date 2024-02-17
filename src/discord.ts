2fb3abe632c81d54cefcbce17481a8f1c7c34632
bvpav <78971592+bvpav@users.noreply.github.com>
2024-02-17 03:09:38 +0200

    renamed `state` -> `signature`

--- src/discord.ts
+++ src/discord.ts
@@ -5,12 +5,15 @@ import { env } from "~/env";
 const DISCORD_AUTHORIZATION_URL = "https://discord.com/oauth2/authorize";
 export const DISCORD_CALLBACK_PATH = "/api/oauth2/discord/callback";
 
-export const getDiscordAuthorizationUrl = (req: NextRequest, state: string) =>
+export const getDiscordAuthorizationUrl = (
+  req: NextRequest,
+  signature: string,
+) =>
   `${DISCORD_AUTHORIZATION_URL}?${new URLSearchParams([
     ["response_type", "code"],
     ["client_id", env.DISCORD_CLIENT_ID],
     ["scope", "identify guilds.join"],
     ["redirect_uri", absoluteUrl(DISCORD_CALLBACK_PATH, req).toString()],
     ["prompt", "none"],
-    ["state", state],
+    ["state", signature],
   ])}`;
