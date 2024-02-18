5ec72444a9c2801ceae3a447fee4e91259db4483
bvpav <78971592+bvpav@users.noreply.github.com>
2024-02-18 19:23:43 +0200

    init shadcn/ui

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,36 +1,6 @@
-import { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-export function absoluteUrl(url: string | URL, req: NextRequest) {
-  const base = new URL(req.nextUrl.href);
-  return new URL(url, base);
-}
-
-export function relativeErrorUrl(error?: string) {
-  if (!error) {
-    return "/error";
-  }
-  return `/error/${error}`;
-}
-
-export function errorUrl(req: NextRequest, error?: string) {
-  return absoluteUrl(relativeErrorUrl(error), req);
-}
-
-/**
- * Hackily get access to a {@link NextRequest} object for the current invocation
- * of a server component or action.
- */
-export function request() {
-  // HACK: trying Origin header
-  const origin = headers().get("origin");
-  if (origin) {
-    return new NextRequest(origin);
-  }
-  // HACK: using Host header
-  const host = headers().get("host");
-  if (!host) {
-    throw new Error("No host header");
-  }
-  return new NextRequest(`https://${host}`);
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
