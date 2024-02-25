3e79eed5ee767da6381597a212a7cc9045b913aa
bvpav <78971592+bvpav@users.noreply.github.com>
2024-02-25 15:45:38 +0200

    added redirect to ht-web on 404

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,16 @@ const jiti = createJITI(new URL(import.meta.url).pathname);
 jiti("./src/env");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    redirects() {
+        return [
+            {
+                source: "/",
+                destination: "https://hacktues.bg/",
+                permanent: true
+            }
+        ]
+    }
+};
 
 export default nextConfig;
